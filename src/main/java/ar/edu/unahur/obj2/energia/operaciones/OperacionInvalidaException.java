9257063768203286a085421239bc367ffc3dfc5b
CreadorSW <17933209+CreadorSW@users.noreply.github.com>
2026-07-03 20:01:07 -0300

    agregar excepción para rutina

---
+++ src/main/java/ar/edu/unahur/obj2/energia/operaciones/OperacionInvalidaException.java
@@ -0,0 +1,10 @@
+package ar.edu.unahur.obj2.energia.operaciones;
+
+/**
+ * OperacionInvalidaException
+ */
+public class OperacionInvalidaException extends RuntimeException {
+    public OperacionInvalidaException(String mensaje) {
+        super(mensaje);
+    }
+}
